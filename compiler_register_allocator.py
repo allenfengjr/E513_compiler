21dcbbd96a591433f1c38adee63df0d8a3ae359e
Dylan1229 <yvanphys@gmail.com>
2023-09-18 15:13:51 -0400

    Uncover live, build inference and allocate regis

--- compiler_register_allocator.py
+++ compiler_register_allocator.py
@@ -15,14 +15,62 @@ class Compiler(compiler.Compiler):
 
     def read_vars(self, i: instr) -> Set[location]:
         # YOUR CODE HERE
+        # how to include addq, subq, movq?
+        if isinstance(i, Instr):
+            read_set = set()
+            for arg in [Instr.source, Instr.target]:
+                if isinstance(arg, Reg):
+                    read_set.add(arg.id)
+                elif isinstance(arg, Variable):
+                    read_set.add(arg.id)
+        # what about negq?
+        # elif isinstance(i, negq): 
+            return read_set
+
+        # Callq should include all the arguments-passing registers in read_set
+        elif isinstance(i, Callq):
+            read_set = set()
+            argument_passing_registers = ['rdi', 'rsi', 'rdx', 'rcx', 'r8', 'r9']
+            for arg in Callq.func:
+                if isinstance(arg, Reg) in argument_passing_registers:
+                    read_set.add(arg.id)
+                elif isinstance(arg, Variable):
+                    read_set.add(arg.id)    
+            return read_set
         pass
 
     def write_vars(self, i: instr) -> Set[location]:
         # YOUR CODE HERE
+        #Only focus on writeen variables
+        if isinstance(i, Instr):
+            write_set = set()
+            for arg in [Instr.source, Instr.target]:
+                if isinstance(arg, Reg):
+                    write_set.add(arg.id)
+            return write_set
+
+        elif isinstance(i, Callq):
+        # Handle write set for callq instruction (caller-saved registers)
+            caller_saved_registers = ['rax', 'rcx', 'rdx', 'rsi', 'rdi', 'r8', 'r9', 'r10', 'r11']
+            return set(caller_saved_registers)
         pass
 
     def uncover_live(self, p: X86Program) -> Dict[instr, Set[location]]:
         # YOUR CODE HERE
+        live_after_sets = {}  # Dictionary to store live-after sets for each instruction
+        for i in p.body:
+            live_after_sets[i] = set()
+
+        # Iterate through the instructions in reverse order   
+        for i in reversed(p.body):
+            read_set = self.read_vars(i)
+            write_set = self.write_vars(i)
+
+        # calculate live-after sets
+        live_after_i = (live_after_sets[i]-write_set) | read_set
+
+        live_after_sets[i] = live_after_i
+        return live_after_sets
         pass
 
     ############################################################################
@@ -32,11 +80,36 @@ class Compiler(compiler.Compiler):
     def build_interference(self, p: X86Program,
                            live_after: Dict[instr, Set[location]]) -> UndirectedAdjList:
         # YOUR CODE HERE
+        interference_graph = UndirectedAdjList()
+
+        for i in p.body:
+            write_set = self.write_vars(i)
+            
+            if isinstance(i, Instr('Movq')):
+                #rule1: movq instruction
+                for d in write_set:
+                    for v in live_after[i]:
+                        if v!=d and v!=i.source() and d!=i.target():
+                            interference_graph.add_edge(d, v)
+            else:
+                #rule2: other instructions
+                for d in write_set:
+                    for v in live_after[i]:
+                        if v!=d:
+                            interference_graph.add_edge(d, v)
+
+        return interference_graph
         pass
     
     def build_move(self, p:X86Program) -> UndirectedAdjList:
         # THIS IS FOR CHALLANGE, should build a move graph to move bias
         # this should be called in `allocate_registers`
+        # YOUR CODE HERE
+        move_graph = UndirectedAdjList()
+        for i in p.body:
+            if isinstance(i, Instr('Movq')): #Connect all the vertax in movq instruction
+                move_graph.add_edge(i.source(), i.target())
+        return move_graph
         pass
     
     ############################################################################
@@ -47,12 +120,104 @@ class Compiler(compiler.Compiler):
     def color_graph(self, graph: UndirectedAdjList,
                     variables: Set[location]) -> Tuple[Dict[location, int], Set[location]]:
         # YOUR CODE HERE
+        color_assignment = {} # Mapping of variables to colors
+        saturation_degree = {} # Saturation degree of each vertex
+        vertex_queue = PriorityQueue(less=lambda v1, v2: saturation_degree[v1] < saturation_degree[v2]) # Priority queue of vertices
+
+        # Initialize the saturation degree of each vertex and add them to the queue
+        for v in graph.vertices():
+            saturation_degree[v] = 0
+            vertex_queue.push(v)
+
+        while not vertex_queue.empty():
+            u = vertex_queue.pop()
+            available_colors = set(range(0, 11)) # Set of available colors, other 5 are not used for register allocation
+            # check the neiborhood of u, and remove the color from available colors
+            for v in graph.adjacent(u):
+                if v in color_assignment:
+                    available_colors.discard(color_assignment[v])
+                else:
+                    saturation_degree[v] += 1
+            if available_colors:
+                color = min(available_colors) # choose the smallest color
+                color_assignment[u] = color
+
+        return color_assignment
         pass
 
     def allocate_registers(self, p: X86Program,
                            graph: UndirectedAdjList) -> X86Program:
         # YOUR CODE HERE
+        # Use color_graph to obtain variable-to-color mapping
+        variables = set()
+        for instr in p.body:
+            variables.update(self.read_vars(instr)) | self.write_vars(instr)
+        color_assignment = self.color_graph(graph, variables)
+
+        # Define the correspondence between the colors and Registers
+        register_mapping = {
+            0: 'rcx', 1: 'rdx', 2: 'rsi', 3: 'rdi',
+            4: 'r8', 5: 'r9', 6: 'r10', 7: 'rbx',
+            8: 'r12', 9: 'r13', 10: 'r14'
+        }
+
+        # Replace variables with registers based on the color assignment
+        for instr in p.body:
+            for arg in [instr.source, instr.target]:
+                if arg and isinstance(arg, Variable):
+                    color = cor_assignment.get(arg.id)
+                    if color is not None:
+                        register_name = register_mapping.get(color)
+                        if register_name:
+                            arg.id = register_name  # Replace the variable with the corresponding register
+        return p
         pass
+
+
+############################################################################
+# Considering move graph?
+########################################################################### 
+    def allocate_registers(self, p: X86Program, interference_graph: UndirectedAdjList) -> X86Program:
+        # Create the move graph
+        move_graph = self.build_move(p)
+
+        # Obtain a variable-to-color mapping for non-move-related variables
+        variables = set()
+        for instr in p.body:
+            variables.update(self.read_vars(instr) | self.write_vars(instr))
+        color_assignment = self.color_graph(interference_graph, variables)
+
+        # Define the correspondence between the colors and Registers
+        register_mapping = {
+            0: 'rcx', 1: 'rdx', 2: 'rsi', 3: 'rdi',
+            4: 'r8', 5: 'r9', 6: 'r10', 7: 'rbx',
+            8: 'r12', 9: 'r13', 10: 'r14'
+        }
+
+        # Replace variables with registers based on the color assignment
+        for instr in p.body:
+            for arg in [instr.source, instr.target]:
+                if arg and isinstance(arg, Variable):
+                    color = color_assignment.get(arg.id)
+                    if color is not None:
+                        register_name = register_mapping.get(color)
+                        if register_name:
+                            arg.id = register_name  # Replace the variable with the corresponding register
+
+        # Bias move-related variables
+        for move_edge in move_graph.edges():
+            u, v = move_edge
+            if isinstance(u, Variable) and isinstance(v, Variable):
+                u_color = color_assignment.get(u.id)
+                v_color = color_assignment.get(v.id)
+                if u_color is not None and v_color is not None:
+                    # If both u and v have colors, and they do not interfere, bias them to have the same color
+                    if not interference_graph.has_edge(u, v):
+                        color_assignment[u.id] = v_color
+                    elif not interference_graph.has_edge(v, u):
+                        color_assignment[v.id] = u_color
+
+        return p
 
     ############################################################################
     # Assign Homes
